bbeed2a2f1f28f3ae14703cad4969799da0c94d7
sarfuddinsam <151197851+sarfuddinsam@users.noreply.github.com>
2023-12-09 09:50:26 +0600

    Add files via upload

---
+++ resources/views/home/arrival.blade.php
@@ -0,0 +1,23 @@
+<section class="arrival_section">
+         <div class="container">
+            <div class="box">
+               <div class="arrival_bg_box">
+                  <img src="images/arrival-bg.png" alt="">
+               </div>
+               <div class="row">
+                  <div class="col-md-6 ml-auto">
+                     <div class="heading_container remove_line_bt">
+                        <h2>
+                           #NewPlants
+                        </h2>
+                     </div>
+                     <p style="margin-top: 20px;margin-bottom: 30px;">
+                     Introducing the Monstera Deliciosa, a botanical masterpiece that effortlessly blends grace with greenery. This iconic plant, also known as the Swiss Cheese Plant, boasts large, fenestrated leaves that create a striking visual appeal in any space. With its tropical origin and distinctive perforations, the Monstera Deliciosa is a symbol of natural elegance and sophistication.                     </p>
+                     <a href="{{url('show_products')}}">
+                     Shop Now
+                     </a>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
